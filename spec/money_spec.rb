a9032594cbc3640ccb063218ab59c4d7094633ff
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-07-11 14:46:29 +0100

    Franc by duplication

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -17,6 +17,36 @@ class Dollar
   end
 end
 
+class Franc
+  attr_reader :amount
+  protected :amount
+  
+  def initialize(amount)
+    @amount = amount
+  end
+  
+  def *(multiplier)
+    Franc.new(@amount * multiplier)
+  end
+  
+  def ==(other)
+    amount == other.amount
+  end
+end
+
+describe Franc do
+  it "can be checked for equality" do
+    Franc.new(5).should eq(Franc.new(5))
+    Franc.new(5).should_not eq(Franc.new(6))
+  end
+  
+  it "can be multiplied" do
+    five = Franc.new(5)
+    (five * 2).should eq(Franc.new(10))
+    (five * 3).should eq(Franc.new(15))
+  end
+end
+
 describe Dollar do
   it "can be checked for equality" do
     Dollar.new(5).should eq(Dollar.new(5))
