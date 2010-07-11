25b29ae99cb31397b6291cdbdb0930a9d89f0573
Ashley Moran <ashley.moran@patchspace.co.uk>
2010-07-11 18:25:32 +0100

    Get rate from the bank properly, test for identity rates

--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -37,12 +37,17 @@ class Money
 end
 
 class Bank
+  def initialize
+    @rates = { }
+  end
+  
   def add_rate(from, to, rate)
-    # I didn't see this in the text! But need it to go green...
+    @rates[ [from, to] ] = rate
   end
   
   def rate(from, to)
-    from == :CHF && to == :USD ? 2 : 1
+    return 1 if from == to
+    @rates[ [from, to] ]
   end
   
   def reduce(source, options)
@@ -69,6 +74,13 @@ describe Bank do
   it "reduces sums" do
     sum = Sum.new(Money.dollar(3), Money.dollar(4))
     bank.reduce(sum, to: :USD).should eq(Money.dollar(7))
+  end
+  
+  describe "rates" do
+    it "has an identity rate" do
+      bank.add_rate(:CHF, :USD, 2)
+      bank.rate(:USD, :USD).should eq(1)
+    end
   end
   
   describe "reducing Money" do
